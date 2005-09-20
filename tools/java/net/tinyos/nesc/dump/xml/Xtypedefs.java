fef0c3ef3fccf27986f9d91d3437e4ac34747570
idgay <idgay>
2005-09-20 17:37:20 +0000

    missing classes for new elements

---
+++ tools/java/net/tinyos/nesc/dump/xml/Xtypedefs.java
@@ -0,0 +1,28 @@
+// $Id$
+/*									tab:4
+ * Copyright (c) 2004-2005 Intel Corporation
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached INTEL-LICENSE     
+ * file. If you do not find these files, copies can be found by writing to
+ * Intel Research Berkeley, 2150 Shattuck Avenue, Suite 1300, Berkeley, CA, 
+ * 94704.  Attention:  Intel License Inquiry.
+ */
+
+package net.tinyos.nesc.dump.xml;
+
+import org.xml.sax.*;
+import java.util.*;
+
+/**
+ * Top-level interface definition list.
+ */
+public class Xtypedefs extends NDList
+{
+    public static LinkedList/*Xtypedef*/ list;
+
+    public NDElement end() {
+	list = l;
+	return this;
+    }
+}
