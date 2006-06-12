a703b8fd30396c38ed79eeeca7711e47d4c8817e
idgay <idgay>
2006-06-12 23:32:46 +0000

    Fix wiring checks. The rules were bad, replace with: - if interface is provided, count edges from modules to interface - if interface is used, count edges from interface to modules

--- tools/java/net/tinyos/nesc/wiring/WiringCheck.java
+++ tools/java/net/tinyos/nesc/wiring/WiringCheck.java
@@ -24,23 +24,10 @@ import org.xml.sax.*;
 */
 public class WiringCheck
 {
-    /* Returns true if intf contains commands (when wantCommands = true)
-       or events (when wantCommands = false) */
-    boolean contains(Xinterface intf, boolean wantCommands) {
-	Xinterfacedef idef = (Xinterfacedef)intf.instance.parent;
-	ListIterator fns = idef.functions.listIterator();
-
-	while (fns.hasNext()) {
-	    Xfunction fn = (Xfunction)fns.next();
-
-	    if (fn.command == wantCommands)
-		return true;
-	}
-	return false;
-    }
+    final static boolean DEBUG = false;
 
     boolean check1Wire(WiringScan from, int min, int max) {
-	int count = count(from);
+	int count = count(0, from);
 
 	if (min >= 0 && count < min) 
 	    return explain("underwired", from);
@@ -58,7 +45,7 @@ public class WiringCheck
 
     /* We know the wiring graph is acyclic */
 
-    int count(WiringScan position) {
+    int count(int indent, WiringScan position) {
 	ListIterator out = position.edges();
 	int count = 0;
 	WiringScan temp = null;
@@ -73,10 +60,14 @@ public class WiringCheck
 	    if (temp.follow(e)) {
 		if (inModule(temp))
 		    count++;
-		count += count(temp);
+		count += count(indent + 1, temp);
 	    }
 	}
-	//System.err.println("fcount " + count + " @ " + position);
+	if (DEBUG) {
+	    for (int i = 0; i < indent; i++)
+		System.err.print("  ");
+	    System.err.println("fcount " + count + " @ " + position);
+	}
 
 	return count;
     }
@@ -136,15 +127,18 @@ public class WiringCheck
 	    if (min == -1 && max == -1)
 		continue;
 
-	    boolean providing = contains(check1, check1.provided);
-	    boolean using = contains(check1, !check1.provided);
 	    WiringNode checkNode = Xwiring.wg.lookup(check1);
 
-	    //System.err.println("min " + min + ", max " + max + " " + providing + " " + using);
-	    if (providing)
+	    if (check1.provided) {
+		if (DEBUG)
+		    System.err.println("P: min " + min + ", max " + max);
 		reported = check1Wire(new WiringScanBackwards(checkNode), min, max);
-	    if (using && !reported)
+	    }
+	    else {
+		if (DEBUG)
+		    System.err.println("U: min " + min + ", max " + max);
 		check1Wire(new WiringScanForwards(checkNode), min, max);
+	    }
 	}
     }
 
